3be45b33960079c04d0e45a37c68180884fd15b1
brenai-works <d01520417@gmail.com>
2025-01-12 12:51:17 +1000

    added code for preparing data for analysis

--- src/scripts/evaluator_interrater_reliability.py
+++ src/scripts/evaluator_interrater_reliability.py
@@ -1,10 +1,15 @@
 # core
+import numpy as np
+import pandas as pd
 import sys, getopt
 
 # pipes
 from src.pipes._get_responses import read_human_rater_responses
 from src.pipes._get_responses import read_llm_rater_responses
 from src.pipes.show_results import show_low_aggreement_papers_with
+
+# utils
+from src.utils.prepare_data import prepare_dataset_for_aggreement 
 
 # main
 def main(argv):
@@ -56,6 +61,7 @@ def main(argv):
     df_llm = read_llm_rater_responses(prt=True)
 
     # calculate the aggreement levels for each human and llm responses (matched by paper title & authors)
+    reference, data = prepare_dataset_for_aggreement(prt=True, _human=df_human, _llm=df_llm)
 
     # calculate Kappa's statistic 
 
@@ -63,7 +69,7 @@ def main(argv):
 
     # show records/papers to human agents for further group discussion
     if show_low_aggree.lower() == "y": 
-        show_low_aggreement_papers_with(scores_under=0.60, dataset=[])
+        show_low_aggreement_papers_with(scores_under=0.60, dataset=data)
         print("")
         sys.exit() 
     elif show_low_aggree.lower() == "n" or show_low_aggree.lower() == "":
--- src/utils/prepare_data.py
+++ src/utils/prepare_data.py
@@ -2,5 +2,28 @@
 import numpy as np
 import pandas as pd
 
-def prepare_dataset(_human, _llm):
+def prepare_dataset_for_aggreement(prt, _human, _llm):
+    id = 0
+    ref = pd.DataFrame.from_dict({"id":[], "title":[]})
+    _id = []
+    _titles = []
+    _h_include = []
+    _llm_include = []
+    val = []
+    for i in range(len(_human)):
+        for x in range(len(_llm)):
+            if _human.iloc[i]["title"] == _llm.iloc[x]["title"] and _human.iloc[i]["authors"] == _llm.iloc[x]["authors"]:
+                _id.append(id)
+                _titles.append(_human.iloc[i]["title"])                
+                _h_include.append(_human.iloc[i]["included"])
+                _llm_include.append(_llm.iloc[x]["included"])
+                id = id + 1
+    ref["id"] = _id
+    ref["title"] = _titles
+    val.append(_h_include)
+    val.append(_llm_include)
+    data = np.array(val)
     print(">> prepare dataset for analysis [...]")
+    if prt:
+        print(ref)
+    return ref, data
